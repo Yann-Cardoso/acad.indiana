f6071d2448bf31da825afc770ae0d7488e934ab8
Yann-Cardoso <yannlucas2002c@gmail.com>
2023-08-06 23:19:52 -0300

    Create atividade,4.php

---
+++ atividade,4.php
@@ -0,0 +1,23 @@
+<?php
+$sal1 = 5000;
+$sal2 = 2500;
+$aumento1 = 0.10; // 10% de aumento
+$aumento2 = 0.20; // 20% de aumento
+
+$
+$anos = 0;
+
+while ($sal2 <= $sal1) {
+    
+   
+$sal1 += $sal1 * $aumento1;
+    
+   
+$sal2 += $sal2 * $aumento2;
+    
+   
+$anos++;
+}
+
+echo "Seu colega irá ultrapassar o seu salário em $anos anos.";
+?>
